78595d0352ceeaee71af1daaa7452e59d8dc7481
Austin Huertas <Austin.huertas@proton.me>
2026-02-01 15:17:58 -0500

    Module 4 completed baseline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); 
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -11,11 +11,13 @@ var handlebars = require('hbs');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//view engine setup to point to app_server/views
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+// var apiRouter = require('./app_api/routes/index'); 
+// app.use('/api', apiRouter);
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
+// register handlebars partials
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
@@ -35,11 +37,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
